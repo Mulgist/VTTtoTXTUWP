9005a3f7fc72c2dc86193aec4fc19ca13a18e28e
Juwon Lee <good9797@outlook.com>
2017-12-31 01:37:46 +0900

    TXT 저장 처리 완료

--- VTTtoTXTUWP/MainPage.xaml.cs
+++ VTTtoTXTUWP/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using VTTtoTXTUWP.Views;
 
 // 빈 페이지 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x412에 나와 있습니다.
 
@@ -25,6 +26,28 @@ namespace VTTtoTXTUWP
         public MainPage()
         {
             this.InitializeComponent();
+            MainSplitView.IsPaneOpen = false;
+            App.titleStack.Push("홈");
+            Title.Text = App.titleStack.Peek();
+            (MainSplitView.Content as Frame).Navigate(typeof(HomeView));
+
         }
+
+        private void HambergerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;
+        }
+
+        private void IconsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BottomIconsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        
     }
 }
---
+++ VTTtoTXTUWP/Views/HomeView.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+// 빈 페이지 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=234238에 나와 있습니다.
+
+namespace VTTtoTXTUWP.Views
+{
+    /// <summary>
+    /// 자체적으로 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
+    /// </summary>
+    public sealed partial class HomeView : Page
+    {
+        StorageFile file;
+        String fileName = "";
+
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+
+        private async void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            await FilePickAsync();
+        }
+
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            await FileSaveAsync();
+        }
+
+        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async Task FilePickAsync()
+        {
+            var open = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = PickerLocationId.PicturesLibrary
+            };
+            open.FileTypeFilter.Add(".vtt");
+            open.FileTypeFilter.Add("*");
+            
+
+            file = await open.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                // 파일 로드됨
+                FilePathText.Text = file.Path;
+                fileName = file.Name;
+            }
+        }
+
+        private async Task FileSaveAsync()
+        {
+            var save = new FileSavePicker
+            {
+                // save. = PickerViewMode.Thumbnail;
+                SuggestedStartLocation = PickerLocationId.PicturesLibrary
+            };
+            save.FileTypeChoices.Add("텍스트 파일", new List<String> { ".txt" });
+            save.FileTypeChoices.Add("모든 파일", new List<String> { "." });
+            save.DefaultFileExtension = ".txt";
+
+            //save.SuggestedSaveFile = saveFile;
+            save.SuggestedFileName = fileName;
+            
+            var saveFile = await save.PickSaveFileAsync();
+            var lineList = textRead(TxtText.Text);
+
+            await FileIO.WriteLinesAsync(saveFile, lineList);
+        }
+
+        private List<String> textRead(string text)
+        {
+            List<String> list = new List<string> { };
+            string line;
+            StringReader reader = new StringReader(text);
+            while (true)
+            {
+                line = reader.ReadLine();
+                if (line != null)
+                {
+                    list.Add(line);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return list;
+        }
+    }
+
+}
